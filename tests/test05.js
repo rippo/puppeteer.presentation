158768a9c89673bdc4fc2635ac352f3be0643be6
Richard Wilde <richard@wildesoft.net>
2018-01-24 18:48:37 +0000

    Ready for demo test05

--- tests/test05.js
+++ tests/test05.js
@@ -1,16 +1,16 @@
 const {expect } = require('chai'); 
 const {test } = require('../browser'); 
-const path = require('path'); 
 const options = require('../options');
+const path = require('path'); 
 const looksSame = require('looks-same');
 
 describe('When I take a screenshot of the home page', () =>  {
 
     //Image we are going to download
-    var image1 = path.join(__dirname, '../images/home.png'); 
+    let image1 = path.join(__dirname, '../images/home.png'); 
 
     //The image we are going to compare against
-    var image2 = path.join(__dirname, '../images/ORG-home-headless.png'); 
+    let image2 = path.join(__dirname, '../images/ORG-home-headless.png'); 
 
     //There seems to be a difference between chromium in headless
     //  and normal mode. So if showing the browser use the non
@@ -21,7 +21,7 @@ describe('When I take a screenshot of the home page', () =>  {
 
     //save home page as an image
     it('it returns a buffer', test(async (browser, opts) =>  {
-        var page = await browser.newPage(); 
+        let page = await browser.newPage(); 
         await page.goto(`${opts.appUrl}`); 
         await page.waitFor('h1'); 
         const screen = await page.screenshot( {path:'./images/home.png'}); 
